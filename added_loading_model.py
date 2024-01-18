dd4930e8984b6296913aeb77f69cab57a7e6bcbf
staticpunch <134955821+staticpunch@users.noreply.github.com>
2024-01-18 10:39:09 +0700

    Create added_loading_model.py

---
+++ added_loading_model.py
@@ -0,0 +1,78 @@
+    #### LOAD MODEL ######
+    # model = INSTRUCTOR(real_name_or_path, cache_folder=model_args.cache_dir)
+    compute_dtype = (torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else torch.float32))
+    if training_args.load_in_kbits in [4, 8]:
+        load_in_4bit = training_args.load_in_kbits == 4
+        load_in_8bit = training_args.load_in_kbits == 8
+        if training_args.modules_to_save is not None:
+            load_in_8bit_skip_modules = training_args.modules_to_save.split(',')
+        else:
+            load_in_8bit_skip_modules = None
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=training_args.load_in_kbits == 4,
+            load_in_8bit=training_args.load_in_kbits == 8,
+            llm_int8_threshold=6.0,
+            load_in_8bit_skip_modules=load_in_8bit_skip_modules,
+            bnb_4bit_compute_dtype=compute_dtype,
+            bnb_4bit_use_double_quant=training_args.double_quant,
+            bnb_4bit_quant_type=training_args.quant_type # {'fp4', 'nf4'}
+        )
+    else:
+        load_in_4bit = False
+        load_in_8bit = False
+        quantization_config = None
+    if quantization_config is not None:
+        print(f"quantization_config:{quantization_config.to_dict()}")
+        
+    device_map = {"":int(os.environ.get("LOCAL_RANK") or 0)}
+    model = LlamaForSequenceEmbedding.from_pretrained(
+        model_args.model_name_or_path,
+        torch_dtype=torch.bfloat16,
+        low_cpu_mem_usage=True,
+        device_map=device_map,
+        load_in_4bit=load_in_4bit,
+        load_in_8bit=load_in_8bit,
+        quantization_config=quantization_config,
+        use_flash_attention_2=training_args.use_flash_attention_2
+    )
+    if not training_args.full_finetuning:
+        if training_args.peft_path is not None:
+            logger.info("Peft from pre-trained model")
+            model = PeftModel.from_pretrained(model, training_args.peft_path, device_map=device_map)
+        else:
+            logger.info("Init new peft model")
+            target_modules = training_args.trainable.split(',')
+            modules_to_save = training_args.modules_to_save
+            if modules_to_save is not None:
+                modules_to_save = modules_to_save.split(',')
+            lora_rank = training_args.lora_rank
+            lora_dropout = training_args.lora_dropout
+            lora_alpha = training_args.lora_alpha
+            logger.info(f"target_modules: {target_modules}")
+            logger.info(f"lora_rank: {lora_rank}")
+            peft_config = LoraConfig(
+                task_type=TaskType.CAUSAL_LM,
+                target_modules=target_modules,
+                inference_mode=False,
+                r=lora_rank, lora_alpha=lora_alpha,
+                lora_dropout=lora_dropout,
+                modules_to_save=modules_to_save)
+            model = get_peft_model(model, peft_config)
+        model.print_trainable_parameters()
+        logger.info(f"model.modules_to_save: {model.modules_to_save}")
+        old_state_dict = model.state_dict
+        model.state_dict = (
+            lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
+        ).__get__(model, type(model))
+    if not training_args.full_finetuning and training_args.gradient_checkpointing and \
+        (not model.modules_to_save or 'embed_tokens' not in model.modules_to_save):
+        # enable requires_grad to avoid exception during backward pass when using gradient_checkpoint without tuning embed.
+        if hasattr(model.base_model, "enable_input_require_grads"):
+            model.base_model.enable_input_require_grads()
+        elif hasattr(model.base_model, "get_input_embeddings"):
+            def make_inputs_require_grad(_module, _input, _output):
+                _output.requires_grad_(True)
+            model.base_model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
+    
+    # import pdb; pdb.set_trace()
+    #### DONE LOADING MODEL 
